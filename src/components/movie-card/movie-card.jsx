58105c8f94bc4906f8e5bdf7dfb491e2440e6239
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2022-01-04 16:33:54 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,27 +3,36 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import { Link } from "react-router-dom";
+
 export class MovieCard extends React.Component {
-    render() {
-      const { movie, onMovieClick } = this.props;
-  
-      return (
-        <Card>
-          <Card.Img variant="top" src={movie.imagePath} />
-          <Card.Body>
-            <Card.Title>{movie.title}</Card.Title>
-            <Card.Text>{movie.description}</Card.Text>
-            <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
-          </Card.Body>
-        </Card>
-      );
-    }
+  render() {
+    const { movie } = this.props;
+
+    return (
+      <Card>
+        <Card.Img variant="top" src={movie.imagePath} />
+        <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{movie.description}</Card.Text>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="link">Open</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    );
   }
+}
 MovieCard.propTypes = {
     movie: PropTypes.shape({
       title: PropTypes.string.isRequired,
       description: PropTypes.string.isRequired,
-      imagePath: PropTypes.string.isRequired
+      imagePath: PropTypes.string.isRequired,
+        director: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            bio: PropTypes.string.isRequired,
+            birth: PropTypes.string.isRequired,
+        })
     }).isRequired,
     onMovieClick: PropTypes.func.isRequired
   };

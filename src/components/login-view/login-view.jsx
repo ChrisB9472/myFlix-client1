c12614ff0cee6f855570faa8f0dadc42f30eca72
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2022-01-14 14:52:34 -0500

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-import PropTypes from 'prop-types';
+
 
 export function LoginView(props) {
   const [username, setUsername] = useState('');

677ffb69a928cc8c007df78622fcb4f02fec5fc6
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2022-01-14 14:52:41 -0500

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 
 import { Link } from "react-router-dom";
 
 export class MovieView extends React.Component {
 
+  addFavoriteMovie() {
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('user');
+
+    axios.post(`https://evening-caverns-13073.herokuapp.com/users/${username}/movies/${this.props.movie._id}`, {}, {
+        headers: { Authorization: `Bearer ${token}` },
+        method: 'POST'
+    })
+        .then(response => {
+            alert(`Added to Favorites List`)
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
+
     render() {
         const { movie, onBackClick } = this.props;
+
+      
     
         return (
           <div className="movie-view">
@@ -23,19 +41,14 @@ export class MovieView extends React.Component {
               <span className="label">Description: </span>
               <span className="value">{movie.description}</span>
             </div>
-          
             <Link to={`/`}>
             <Button variant="link">Back</Button>
           </Link>
           <Link to={`/directors/${movie.director.name}`}>
   <Button variant="link">Director</Button>
+  <Button variant="outline-primary" className="btn-outline-primary" value={movie._id} onClick={(e) => this.addFavoriteMovie(e, movie)}>Add to Favorites</Button>
 </Link>
 
-<Link to={`/genres/${movie.genre.name}`}>
-  <Button variant="link">Genre</Button>
-</Link>
-            
-    
           </div>
         );
       }

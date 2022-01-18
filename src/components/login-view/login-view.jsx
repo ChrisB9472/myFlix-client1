72f7a9756870ff728bd188731e6b578d3da8bb19
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2022-01-18 09:15:59 -0500

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,106 +1,51 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import axios from "axios";
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 import { Link } from "react-router-dom";
-import {
-  Form,
-  Button,
-  Card,
-  CardGroup,
-  Container,
-  Col,
-  Row,
-} from "react-bootstrap";
 
 
 
 export function LoginView(props) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
     /* Send a request to the server for authentication */
-    axios
-      .post("https://topimdbmovies.herokuapp.com/login", {
-        Username: username,
-        Password: password,
-      })
-      .then((response) => {
-        const data = response.data;
-        console.log(data);
-        props.onLoggedIn(data);
-      })
-      .catch((e) => {
-        console.log("User does not exist");
-      });
+    axios.post('https://evening-caverns-13073.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    })
+    .then(response => {
+      const data = response.data;
+      props.onLoggedIn(data);
+    })
+    .catch(e => {
+      console.log('no such user')
+    });
   };
 
   return (
-    <>
-      <div className="login-view">
-        <h1 className="intro">Welcome, to My Flix!</h1>
-        <Container>
-          <Row>
-            <Col>
-              <CardGroup>
-                <Card>
-                  <Card.Body>
-                    <Card.Title>Log In</Card.Title>
-                    <Form>
-                      <Form.Group controlId="formUsername">
-                        <span className="subtitle">USERNAME:</span>
-                        <br />
-                        <input
-                          type="text"
-                          placeholder="Enter username"
-                          value={username}
-                          onChange={(e) => setUsername(e.target.value)}
-                        />
-                      </Form.Group>
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+      </Form.Group>
 
-                      <Form.Group controlId="formpassword">
-                        <span className="subtitle">PASSWORD</span>
-                        <br />
-                        <input
-                          type="password"
-                          placeholder="Enter password"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                        />
-                      </Form.Group>
-                      <Button
-                        variant="primary"
-                        type="submit"
-                        onClick={handleSubmit}
-                        style={{ marginTop: "20px" }}
-                      >
-                        Login
-                      </Button>
-                      <p>New to Topimdbmovies? Please sign up!</p>
-                      <Link to="/register">
-                        <Button
-                          style={{ marginTop: "10px" }}
-                          variant="primary"
-                          type="button"
-                        >
-                          {" "}
-                          Create Account
-                        </Button>
-                      </Link>
-                    </Form>
-                  </Card.Body>
-                </Card>
-              </CardGroup>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </>
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
+      <Link to={`/register`}>
+                                <a href="/register">Not a member?</a>
+                            </Link>
+                       
+                    
+    </Form>
+    
   );
 }
-
-LoginView.propTypes = {
-  onLoggedIn: PropTypes.func.isRequired,
-};
